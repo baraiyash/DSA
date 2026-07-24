adc11b13ff69c9aedd32a7a2bb0189fbdfb98f18
Yash Barai <yashhbarai@gmail.com>
2026-07-24 12:18:33 +0530

    Time: 24 ms (26.58%), Space: 81.5 MB (82.27%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,23 @@
 import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
 
-        for(int num: nums){
-            if(set.contains(num)){
+        // for(int num: nums){
+        //     if(set.contains(num)){
+        //         return true;
+        //     }
+        //     set.add(num);
+        // }
+        // return false;
+
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
                 return true;
             }
-            set.add(num);
         }
         return false;
+
     }
 }
