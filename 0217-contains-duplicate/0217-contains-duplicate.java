04e019cf75518a6965cc8915f58d6868e1783f48
Yash Barai <yashhbarai@gmail.com>
2026-07-24 12:18:54 +0530

    Time: 17 ms (61.45%), Space: 111.6 MB (5.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,23 +1,23 @@
 import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        // for(int num: nums){
-        //     if(set.contains(num)){
-        //         return true;
-        //     }
-        //     set.add(num);
-        // }
-        // return false;
-
-        Arrays.sort(nums);
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] == nums[i-1]){
+        for(int num: nums){
+            if(set.contains(num)){
                 return true;
             }
+            set.add(num);
         }
         return false;
+
+        // Arrays.sort(nums);
+        // for(int i=1; i<nums.length; i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
 
     }
 }
