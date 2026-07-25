f35bdc4043682f741db665ad615323f407c592a7
Yash Barai <yashhbarai@gmail.com>
2026-07-26 02:48:29 +0530

    Time: 12 ms (23.29%), Space: 47.3 MB (6.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }
+            else{
+                set.add(num);
+            }
+        }
+        return set.iterator().next();
+    }
+}
